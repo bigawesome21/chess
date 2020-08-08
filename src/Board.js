4d843550c1a1bc68d9021b3a4601652275502ddc
bigawesome21 <chendragon2000@gmail.com>
2020-08-08 14:29:31 -0700

    installed react-chess.js

--- src/Board.js
+++ src/Board.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Chessboard from 'react-simple-chessboard';
+import useChess from 'react-chess.js';
 
 class Board extends React.Component {
   render() { 
     return (
       <Chessboard 
         draggable={true}
-        position={'start'}
-        dropOffBoard={true} /> 
+        position={'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'        }
+        dropOffBoard={true} 
+        onDragStart={'onDragStart'}
+        onDrop={'onDrop'}
+        onSnapEnd={'onSnapEnd'} /> 
     );
   }
 }
